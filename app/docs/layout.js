9776e2c590f508de9f097eeb440c1590b7a5a162
jolethen <155651557+jolethen@users.noreply.github.com>
2026-02-09 19:00:32 +0530

    Create layout.js

---
+++ app/docs/layout.js
@@ -0,0 +1,10 @@
+import Sidebar from "../../components/Sidebar";
+
+export default function DocsLayout({ children }) {
+  return (
+    <div className="docs">
+      <Sidebar />
+      <main className="content">{children}</main>
+    </div>
+  );
+}
