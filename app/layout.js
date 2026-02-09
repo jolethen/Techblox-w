a65bf964da87dbe2084fe2daa63fc69e40275d3b
jolethen <155651557+jolethen@users.noreply.github.com>
2026-02-09 18:58:36 +0530

    Update and rename README.md to app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+# Techblox-w
+import "../styles/globals.css";
+
+export const metadata = {
+  title: "TechBlox Wiki",
+  description: "Official TechBlox Server Documentation",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
