e34423ba875c62915a0686da7aa0b5a85796c324
jolethen <155651557+jolethen@users.noreply.github.com>
2026-02-09 19:03:05 +0530

    Create page.js

---
+++ app/docs/rules/page.js
@@ -0,0 +1,20 @@
+export default function Rules() {
+  return (
+    <>
+      <h1>Server Rules</h1>
+
+      <h2>General</h2>
+      <ul>
+        <li>No hacking or exploits</li>
+        <li>No lag machines</li>
+        <li>Respect staff</li>
+      </ul>
+
+      <h2>PvP</h2>
+      <ul>
+        <li>PvP allowed in war zones</li>
+        <li>No spawn killing</li>
+      </ul>
+    </>
+  );
+    }
