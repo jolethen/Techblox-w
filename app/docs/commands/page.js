bd10fe52e846da8a0f3fe9c8b63c897c8f3e937d
jolethen <155651557+jolethen@users.noreply.github.com>
2026-02-09 19:03:38 +0530

    Create page.js

---
+++ app/docs/commands/page.js
@@ -0,0 +1,21 @@
+export default function Commands() {
+  return (
+    <>
+      <h1>Commands</h1>
+
+      <h2>Player</h2>
+      <pre>
+{`/spawn
+/home
+/tpa`}
+      </pre>
+
+      <h2>Admin</h2>
+      <pre>
+{`/ban
+/kick
+/give`}
+      </pre>
+    </>
+  );
+}
