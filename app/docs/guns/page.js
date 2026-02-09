594791ae797dca62d8cef0e07dab59a5b168529e
jolethen <155651557+jolethen@users.noreply.github.com>
2026-02-09 19:04:06 +0530

    Create page.js

---
+++ app/docs/guns/page.js
@@ -0,0 +1,17 @@
+export default function Guns() {
+  return (
+    <>
+      <h1>Guns & PvP</h1>
+      <p>
+        TechBlox features realistic guns with recoil, ammo,
+        and high-risk combat.
+      </p>
+
+      <ul>
+        <li>Headshots do extra damage</li>
+        <li>Ammo is limited</li>
+        <li>Armor matters</li>
+      </ul>
+    </>
+  );
+}
